759becae5c31cfdc0f1b9146c636beec0a6f3ec6
Thomas Neos <tneos@hotmail.com>
2025-11-10 19:47:19 +0000

    Wrapper for Landing page, initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import Landing from "./pages/Landing";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: green;
-  color: white;
-  font-size: 2rem;
-`;
 
 function App() {
   return (
     <div>
-      <Button>Edit</Button>
       <Landing />
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,10 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
 function Landing() {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobster logo" className="logo" />
       </nav>
@@ -23,7 +24,50 @@ function Landing() {
         {/* main image */}
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
 export default Landing;
