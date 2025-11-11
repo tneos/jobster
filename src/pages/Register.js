33caedb9bc7969cdd0427a06a9578939ff43f245
Thomas Neos <tneos@hotmail.com>
2025-11-11 18:35:17 +0000

    Register component, basic setup, initial commit

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,53 @@
+import {useState, useEffect} from "react";
+import {Logo} from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  // TODO -- redux toolkit and useNavigate
+
+  const handleChange = e => {
+    console.log(e.target);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-name">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 }
 export default Register;
