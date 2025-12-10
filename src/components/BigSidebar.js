da3765d2f132f5f64341059936b78b1ee96e1092
Thomas Neos <tneos@hotmail.com>
2025-12-10 12:42:49 +0000

    Add both sidebars functionality, setup NavLinks component

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,8 +1,21 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import {useSelector} from "react-redux";
+
 const BigSidebar = () => {
+  // Get state value
+  const {isSidebarOpen} = useSelector(store => store.user);
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>
+      <div className={isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"}>
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
