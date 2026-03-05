c0a070f8faa76a450bf2a616fd3f7ec4d458b4fe
Thomas Neos <tneos@hotmail.com>
2026-03-05 18:53:30 +0000

    Add demo user button, read-only data available

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -92,6 +92,14 @@ function Register() {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           {isLoading ? "Loading..." : "Submit"}
         </button>
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          disabled={isLoading}
+          onClick={() => dispatch(loginUser({email: "testUser@test.com", password: "secret"}))}
+        >
+          {isLoading ? "Loading..." : "demo user"}
+        </button>
         <p>
           {values.isMember ? "Not a member yet?" : "Already a member?"}
           <button type="button" className="member-btn" onClick={toggleMember}>
