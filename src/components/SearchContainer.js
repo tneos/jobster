6e2eb4de04f50b02776530969d6f15d5c43e11b6
Thomas Neos <tneos@hotmail.com>
2026-04-23 08:24:01 +0100

    Add handleChange and clearFilters functionality into reducers, update state in SearchContainer

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,6 +1,7 @@
 import {FormRow, FormRowSelect} from ".";
 import Wrapper from "../assets/wrappers/SearchContainer";
 import {useSelector, useDispatch} from "react-redux";
+import {handleChange, clearFilters} from "../features/allJobs/allJobsSlice";
 
 const SearchContainer = () => {
   const {isLoading, search, searchStatus, searchType, sort, sortOptions} = useSelector(
@@ -8,10 +9,14 @@ const SearchContainer = () => {
   );
 
   const {jobTypeOptions, statusOptions} = useSelector(store => store.job);
+  const dispatch = useDispatch();
 
-  const handleSearch = e => {};
+  const handleSearch = e => {
+    dispatch(handleChange({name: e.target.name, value: e.target.value}));
+  };
   const handleSubmit = e => {
     e.preventDefault();
+    dispatch(clearFilters());
   };
   return (
     <Wrapper>
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -53,6 +53,14 @@ const allJobsSlice = createSlice({
     hideLoading: state => {
       state.isLoading = false;
     },
+    handleChange: (state, {payload: {name, value}}) => {
+      // state.page = 1 for pagination
+      state[name] = value;
+    },
+    clearFilters: state => {
+      // Return initial state and filters state values
+      return {...state, ...initialFiltersState};
+    },
   },
   extraReducers: builder => {
     builder
@@ -82,6 +90,6 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const {showLoading, hideLoading} = allJobsSlice.actions;
+export const {showLoading, hideLoading, handleChange, clearFilters} = allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
